e9d2a96c9a0662fcaf43b6d9557b42d1d380f993
whatcost99 <isimdegistir21@gmail.com>
2023-04-23 22:52:46 +0300

    Large One lOL

---
+++ Oladins-Website/dapp.config.js
@@ -0,0 +1,10 @@
+const config = {
+  title: 'OLADINS',
+  description: 'Oladins Web',
+  contractAddress: '0xF68ABb0d485b25968f29291Ec569C5DA29b565A7',
+  maxMintAmount: 10,
+  presaleMaxMintAmount: 1,
+  price: 0.0077
+}
+
+export { config }
